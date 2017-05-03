dfc1960e9f8a98e96b5e422e2c6c34e03a49bdbc
Cethin <cethin@live.com>
2017-05-03 02:16:26 -0400

    Improved BHops

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Controller : MonoBehaviour {
-	protected const string forwardAxis = "Vertical";
-	protected const string rightAxis = "Horizontal";
-	protected const string jumpAxis = "Jump";
-	protected const float horizontalSnapPercent = .5f;
-	protected const float horizontalSpeedPercent = 1f;
-	protected const float maxLateralMagnitude = 50f;
+	protected const string FORWARD_AXIS = "Vertical";
+	protected const string RIGHT_AXIS = "Horizontal";
+	protected const string JUMP_AXIS = "Jump";
+	protected const float HORIZONTAL_SNAP_PERCENT = .5f;
+	protected const float HORIZONTAL_SPEED_PERCENT = 1f;
+	protected const float MAX_LATERAL_MAGNITUDE = 50f;
+	protected const float MAX_ACCELERATION = 50f;
 
-	public float speed = 7f;
+	public float acceleration = 7f;
 	public float jumpForce = .5f;
 	public float xSens = 1f;
 	public float ySens = 1f;
@@ -30,11 +31,15 @@ public class Controller : MonoBehaviour {
 
 	void Update ()
 	{
-		move();
 		rotate();
 		jump();
 
-		capLateralSpeed();
+		//capLateralSpeed();
+	}
+
+	void FixedUpdate()
+	{
+		move();
 	}
 
 	public void lockMouse()
@@ -52,20 +57,34 @@ public class Controller : MonoBehaviour {
 	protected void move()
 	{
 		Vector3 localVel = transform.InverseTransformDirection(rb.velocity);
-		float forwardInput = Input.GetAxis(forwardAxis);
-		if(forwardInput != 0)
+		Vector3 input = transform.TransformDirection(new Vector3(Input.GetAxis(RIGHT_AXIS), 0, Input.GetAxis(FORWARD_AXIS)));
+
+		rb.velocity = Accelerate(input, rb.velocity, acceleration, MAX_ACCELERATION);
+	}
+
+	protected Vector3 accelerate(Vector3 dir, Vector3 vel)
+	{
+		float proj = Vector3.Dot(vel, dir);
+		float accelVel = acceleration * Time.fixedDeltaTime;
+
+		if(proj + accelVel > MAX_ACCELERATION)
 		{
-			localVel.z = speed * forwardInput;
+			accelVel = MAX_ACCELERATION - proj;
 		}
 
-		float rightInput = Input.GetAxis(rightAxis);
-		if(rightInput != 0)
-		{
-			float rightVal = horizontalSpeedPercent * speed * rightInput;
-			localVel.x = rightVal;
-		}
+		return vel + dir * accelVel;
+	}
 
-		rb.velocity = transform.TransformDirection(localVel);
+	private Vector3 Accelerate(Vector3 accelDir, Vector3 prevVelocity, float accelerate, float max_velocity)
+	{
+		float projVel = Vector3.Dot(prevVelocity, accelDir); // Vector projection of Current velocity onto accelDir.
+		float accelVel = accelerate * Time.fixedDeltaTime; // Accelerated velocity in direction of movment
+
+		// If necessary, truncate the accelerated velocity so the vector projection does not exceed max_velocity
+		if(projVel + accelVel > max_velocity)
+			accelVel = max_velocity - projVel;
+
+		return prevVelocity + accelDir * accelVel;
 	}
 
 	protected void rotate()
@@ -79,7 +98,7 @@ public class Controller : MonoBehaviour {
 
 	protected void jump()
 	{
-		if(Input.GetAxisRaw(jumpAxis) != 0)
+		if(Input.GetAxisRaw(JUMP_AXIS) != 0)
 		//if(Input.GetButtonDown(jumpAxis))
 		{
 			if(Physics.Raycast(transform.position, -transform.up, .3f, jumpLM))
@@ -96,9 +115,9 @@ public class Controller : MonoBehaviour {
 	protected void capLateralSpeed()
 	{
 		Vector2 lateralVel = new Vector2(rb.velocity.x, rb.velocity.z);
-		if(lateralVel.magnitude > maxLateralMagnitude)
+		if(lateralVel.magnitude > MAX_LATERAL_MAGNITUDE)
 		{
-			lateralVel = lateralVel.normalized * maxLateralMagnitude;
+			lateralVel = lateralVel.normalized * MAX_LATERAL_MAGNITUDE;
 		}
 
 		rb.velocity = new Vector3(lateralVel.x, rb.velocity.y, lateralVel.y);
