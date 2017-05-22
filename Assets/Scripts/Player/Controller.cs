ed0f59359eb99560efeab0b7d8234d00c571262f
Cethin <cethin@live.com>
2017-05-22 00:39:17 -0400

    Changed bhop to a more fun method.     Look in direction of velocity and strafe to increase speed     Now allows for much more fluid movement and looking in the direction of movement Player friction is now manually calculated. Needed to be changed from default unity physics.     Should account for moving platforms with this change also

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -9,22 +9,29 @@ public class Controller : MonoBehaviour {
 	protected const string JUMP_AXIS = "Jump";
 	protected const float HORIZONTAL_SNAP_PERCENT = .5f;
 	protected const float HORIZONTAL_SPEED_PERCENT = 1f;
-	protected const float MAX_LATERAL_MAGNITUDE = 50f;
-	protected const float MAX_ACCELERATION = 50f;
+	protected const float MAX_LATERAL_MAGNITUDE = 30f;
+	protected const float MAX_ACCELERATION = 10f;
+	protected const float GROUND_RAYCAST_DIST = .3f;
 
 	public float acceleration = 7f;
 	public float jumpForce = .5f;
 	public float xSens = 1f;
 	public float ySens = 1f;
 	public LayerMask jumpLM;
+	public LineRenderer velocityLine;
+	public Vector3 velocityLineOffset;
 
 	protected Rigidbody rb;
 	protected Camera cam;
-	protected bool grounded = true;
+	protected Collider ground;
+
 	void Awake ()
 	{
 		rb = GetComponent<Rigidbody>();
 		cam = GetComponentInChildren<Camera>();
+
+		velocityLine = Instantiate(velocityLine, transform.position, transform.rotation);
+		velocityLine.positionCount = 2;
 
 		lockMouse();
 	}
@@ -32,14 +39,23 @@ public class Controller : MonoBehaviour {
 	void Update ()
 	{
 		rotate();
-		jump();
-
-		//capLateralSpeed();
+		drawNormalizedVelocity();
 	}
 
 	void FixedUpdate()
 	{
+		if(isGrounded())
+		{
+			if(!jump())
+			{
+				applyFriction();
+			}
+		}
+
 		move();
+		
+		capLateralSpeed();
+
 	}
 
 	public void lockMouse()
@@ -57,12 +73,38 @@ public class Controller : MonoBehaviour {
 	protected void move()
 	{
 		Vector3 localVel = transform.InverseTransformDirection(rb.velocity);
-		Vector3 input = transform.TransformDirection(new Vector3(Input.GetAxis(RIGHT_AXIS), 0, Input.GetAxis(FORWARD_AXIS)));
 
-		rb.velocity = Accelerate(input, rb.velocity, acceleration, MAX_ACCELERATION);
+
+		// For Accelerate() and accelerate1()
+		/*
+		Vector3 input = transform.TransformDirection(new Vector3(Input.GetAxis(RIGHT_AXIS), 0, Input.GetAxis(FORWARD_AXIS)));
+		Vector3 velocity = Accelerate(input, rb.velocity, acceleration, MAX_ACCELERATION);
+		*/
+
+		// For accelerate2
+		Vector3 input = new Vector3(Input.GetAxis(RIGHT_AXIS), 0, Input.GetAxis(FORWARD_AXIS));
+		Vector3 velocity = accelerate2(input, localVel);
+
+		// Set velocity
+		rb.velocity = transform.TransformDirection(velocity);
 	}
 
-	protected Vector3 accelerate(Vector3 dir, Vector3 vel)
+	// Draw a line renderer according to the current lateral velocity
+	protected void drawVelocity()
+	{
+		velocityLine.SetPosition(0, transform.position);
+		velocityLine.SetPosition(1, transform.position + new Vector3(rb.velocity.x, 0, rb.velocity.z));
+	}
+
+	// Draw a line renderer according to the normalized current lateral velocity
+	protected void drawNormalizedVelocity()
+	{
+		velocityLine.SetPosition(0, velocityLineOffset + transform.position);
+		velocityLine.SetPosition(1, velocityLineOffset + transform.position + new Vector3(rb.velocity.x, 0, rb.velocity.z).normalized);
+	}
+
+	// First itteration of custom movement
+	protected Vector3 accelerate1(Vector3 dir, Vector3 vel)
 	{
 		float proj = Vector3.Dot(vel, dir);
 		float accelVel = acceleration * Time.fixedDeltaTime;
@@ -75,6 +117,51 @@ public class Controller : MonoBehaviour {
 		return vel + dir * accelVel;
 	}
 
+	// Second iteration of custom movement
+	// Set velocity to movement vector as long as a forward movement is pressed.
+	// OW, if strafe is pressed
+	//		Set strafe to clamped value if below it.
+	//		OR add strafe value.
+	protected Vector3 accelerate2(Vector3 input, Vector3 startVel)
+	{
+		Vector3 vel;
+		if(input.z != 0 && ground != null)
+		{
+			vel = input.normalized * acceleration;
+		}
+		else if(input.x != 0)
+		{
+			vel = startVel;
+
+			// Strafe acceleration
+			Vector3 accel = (input.normalized * (HORIZONTAL_SNAP_PERCENT * acceleration)) * Time.fixedDeltaTime;
+
+			// If less than snap amount, snap horizontal speed to correct value
+			if(Mathf.Abs(vel.x + accel.x) < HORIZONTAL_SNAP_PERCENT * acceleration)
+			{
+				vel.x = (HORIZONTAL_SNAP_PERCENT * acceleration) * (Mathf.Sign(input.x));
+			}
+
+			// OW add strafe acceleration
+			else
+			{
+				vel += accel;
+			}
+		}
+		// If there was no input
+		else
+		{
+			vel = startVel;
+		}
+
+		// Add vertical velocity back
+		vel.y = startVel.y;
+
+		// Return modified velocity value
+		return vel;
+	}
+
+	// Classic Quake movement
 	private Vector3 Accelerate(Vector3 accelDir, Vector3 prevVelocity, float accelerate, float max_velocity)
 	{
 		float projVel = Vector3.Dot(prevVelocity, accelDir); // Vector projection of Current velocity onto accelDir.
@@ -96,20 +183,19 @@ public class Controller : MonoBehaviour {
 		transform.Rotate(new Vector3(0, 1, 0), xRot, Space.World);
 	}
 
-	protected void jump()
+	protected bool jump()
 	{
 		if(Input.GetAxisRaw(JUMP_AXIS) != 0)
 		//if(Input.GetButtonDown(jumpAxis))
 		{
-			if(Physics.Raycast(transform.position, -transform.up, .3f, jumpLM))
-			{
-				Debug.DrawLine(transform.position, transform.position - (transform.up * .5f), Color.red, 1f);
+			Vector3 vel = rb.velocity;
+			vel.y = jumpForce * (-Physics.gravity.y);
+			rb.velocity = vel;
 
-				Vector3 vel = rb.velocity;
-				vel.y = jumpForce * (-Physics.gravity.y);
-				rb.velocity = vel;
-			}
+			return true;
 		}
+
+		return false;
 	}
 
 	protected void capLateralSpeed()
@@ -121,5 +207,40 @@ public class Controller : MonoBehaviour {
 		}
 
 		rb.velocity = new Vector3(lateralVel.x, rb.velocity.y, lateralVel.y);
+	}
+
+	// Applies friction to player. Assumes normal of ground and player are both up
+	protected void applyFriction()
+	{
+		Vector2 vel = new Vector2(rb.velocity.x, rb.velocity.z);
+
+		Rigidbody groundRB = ground.GetComponent<Rigidbody>();
+		Vector2 groundVel = (groundRB == null) ? Vector2.zero : new Vector2(groundRB.velocity.x, groundRB.velocity.z);
+
+		Vector2 friction = Vector3.zero;
+		friction = ground.material.dynamicFriction * (vel - groundVel).normalized;
+
+		// If friction vector is larger than velocity vector, set to 0
+		if(friction.magnitude >= vel.magnitude)
+		{
+			vel = Vector2.zero;
+		}
+		else
+		{
+			vel -= friction;
+		}
+		rb.velocity = new Vector3(vel.x, 0, vel.y);
+	}
+
+	public bool isGrounded()
+	{
+		RaycastHit hit = new RaycastHit();
+		if(Physics.Raycast(transform.position + new Vector3(0, .1f, 0), -transform.up, out hit, GROUND_RAYCAST_DIST, jumpLM))
+		{
+			ground = hit.collider;
+			return true;
+		}
+		ground = null;
+		return false;
 	}
 }
